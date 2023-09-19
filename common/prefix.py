06faaffcad7fe1ececae6a1b4614712540e9abd7
Justin Newberry <jnewberry0502@gmail.com>
2023-09-18 19:53:19 -0700

    prefix alphanumeric

--- common/prefix.py
+++ common/prefix.py
@@ -7,9 +7,14 @@ from typing import Optional
 from openpilot.common.params import Params
 from openpilot.system.hardware.hw import Paths
 
+
+def random_alphanumeric(length):
+  return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+
 class OpenpilotPrefix:
   def __init__(self, prefix: Optional[str] = None, clean_dirs_on_exit: bool = True):
-    self.prefix = prefix if prefix else str(uuid.uuid4().hex[0:15])
+    self.prefix = prefix if prefix else str(random_alphanumeric(16))
     self.msgq_path = os.path.join('/dev/shm', self.prefix)
     self.clean_dirs_on_exit = clean_dirs_on_exit
 

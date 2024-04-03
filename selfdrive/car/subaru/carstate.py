5e069bd33a1c4c9da39b1094c3bfd11f7e82c09e
Jesse Skelton <jesse19skelton@gmail.com>
2024-04-02 18:39:41 -0600

    Update carstate.py
    change CarState to use steering -> steering_angle instead of steering_torque -> steering_angle

--- selfdrive/car/subaru/carstate.py
+++ selfdrive/car/subaru/carstate.py
@@ -46,7 +46,7 @@ class CarState(CarStateBase):
     can_gear = int(cp.vl["Transmission"]["Gear"])
     ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(can_gear, None))
 
-    ret.steeringAngleDeg = cp.vl["Steering_Torque"]["Steering_Angle"]
+    ret.steeringAngleDeg = cp.vl["steering"]["Steering_Angle"]
     ret.steeringTorque = cp.vl["Steering_Torque"]["Steer_Torque_Sensor"]
     ret.steeringTorqueEps = cp.vl["Steering_Torque"]["Steer_Torque_Output"]
 

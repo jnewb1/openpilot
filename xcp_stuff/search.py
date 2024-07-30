30ebfce35c94dacf852855f981195a5a745710b7
Justin Newberry <jnewberry0502@gmail.com>
2024-07-29 22:16:31 -0400

    fix

--- xcp_stuff/search.py
+++ xcp_stuff/search.py
@@ -1,9 +1,9 @@
 
 def read_table(dat, offset_start):
   row1_start = offset_start
-  row2_start = offset_start + 16
-  row1 = [int.from_bytes(dat[row1_start+i*2:row1_start+i*2+1], byteorder="big") for i in range(4)]
-  row2 = [int.from_bytes(dat[row2_start+i*2:row2_start+i*2+1], byteorder="big") for i in range(4)]
+  row2_start = offset_start + 32
+  row1 = [int.from_bytes(dat[row1_start+i*2:row1_start+i*2+2], byteorder="big") for i in range(16)]
+  row2 = [int.from_bytes(dat[row2_start+i*2:row2_start+i*2+2], byteorder="big") for i in range(16)]
 
   return row1, row2
 
@@ -18,12 +18,13 @@ def strictly_increasing(dat):
   return True
 
 
-with open("21_crosstrek.bin", "rb") as f:
+with open("21_legacy.bin", "rb") as f:
   dat = f.read()
 
   for i in range(len(dat)):
     row1, row2 = read_table(dat, i)
 
-    print(row1, row2)
+    if row1[0] != 0 or not strictly_increasing(row1):
+      continue
 
     print(hex(i), row1, row2)
